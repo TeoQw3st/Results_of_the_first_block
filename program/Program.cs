a3ce2ef972e208cbeac1f28b571c11aeeb6c3f89
Pavel <Qw3st@mail.ru>
2023-02-14 18:31:40 +0300

    написал программу

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Создание рандомного массива
+
+
+// функиця показа массива
+void ShowArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string [] massive1 =    {"vvv", "ggg", "111", "ghbdtn"};
+ShowArray (massive1);
+string [] massive2 = new string[massive1.Length];
+int count =0;
+for (int i=0; i<massive1.Length; i++){
+    if (massive1[i].Length<=3){
+        massive2[count]=massive1[i];
+        count++;
+    }
+}
+Console.WriteLine();
+ShowArray (massive2);
